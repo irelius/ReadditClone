3d6a58780a6336e0455ea6cca3cde9b60410d268
irelius <kihong711@hotmail.com>
2025-04-16 01:37:56 -0700

    modal template finished and sign up modal done. css still required

--- react-vite/src/views/MainPage/MainPage.jsx
+++ react-vite/src/views/MainPage/MainPage.jsx
@@ -1,9 +1,23 @@
-import './MainPage.css'
+import "./MainPage.css";
+
+import Modal from "../../components/Modals";
+import { useState } from "react";
+
+import ReactDOM from "react-dom";
+import SignUpForm from "../../components/Modals/SignUpModal/SignUpForm";
 
 export default function MainPage() {
-     return (
-          <div>
-               <section>MainPage Placeholder</section>
-          </div>
-     );
+	const [openModal, setOpenModal] = useState(false);
+
+	return (
+		<div>
+			<button onClick={() => setOpenModal(true)}>show modal</button>
+			<Modal
+				className="test-class"
+				isOpen={openModal}
+				onClose={setOpenModal}
+				children={<SignUpForm onClose={setOpenModal} />}
+			/>
+		</div>
+	);
 }
