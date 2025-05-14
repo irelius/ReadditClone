0995ab6427507ecc2ff1ad42bb27e67d658b7add
irelius <kihong711@hotmail.com>
2025-05-14 05:58:40 -0700

    basic image carousel functionality implemented

--- react-vite/src/components/SinglePost/SinglePost.jsx
+++ react-vite/src/components/SinglePost/SinglePost.jsx
@@ -8,11 +8,8 @@ import { useEffect, useState } from "react";
 export default function SinglePost({ post }) {
 	TimeAgo.addLocale(en);
 
-	if (post.id === 1) {
-		console.log("booba", post);
-	}
-
 	const [load, setLoad] = useState(false);
+	const [imageIndex, setImageIndex] = useState(0);
 
 	useEffect(() => {
 		if (post.id) {
@@ -21,15 +18,19 @@ export default function SinglePost({ post }) {
 	}, []);
 
 	const subreddit = post.subreddits;
-    const imagesById = post.images.images_by_id
-    const images = post.images.images
-
-    if(imagesById.length > 0) {
-        console.log('booba', images)
-    }
-
+	const imagesById = post.images.images_by_id;
+	const images = post.images.images;
+    
 	const timeAgo = new TimeAgo("en-US");
 	const time = timeAgo.format(new Date(post.created_at), "round");
+
+	const imageRotation = (dir) => {
+		if (dir === "left") {
+			setImageIndex((prev) => Math.max(prev - 1, 0));
+		} else if (dir === "right") {
+			setImageIndex((prev) => Math.min(prev + 1, imagesById.length - 1));
+		}
+	};
 
 	return load ? (
 		<div>
@@ -43,24 +44,21 @@ export default function SinglePost({ post }) {
 			</section>
 			<section>{post.title}</section>
 			{/* <section>{post.body}</section> */}
-            {/* {imagesById.length > 0 ? (
-                {imagesById.map(el => {
-                    
-                })}
-                <section>
-
-                </section>
-            ) : (
-                <></>
-            )} */}
-            {imagesById.map(el => {
-                const url = images[el].image_url
-                return (
-                    <section className="post-image-container">
-                        <img className="post-image" src={`${url}`}></img>
-                    </section>
-                )
-            })}
+			<section>
+				{imagesById.length > 0 ? (
+					<section className="post-image-container dfr aic">
+						<aside className="pointer" onClick={() => imageRotation("left")}>
+							{"<"}
+						</aside>
+						<img className="post-image" src={`${images[imagesById[imageIndex]].image_url}`} />
+						<aside className="pointer" onClick={() => imageRotation("right")}>
+							{">"}
+						</aside>
+					</section>
+				) : (
+					<></>
+				)}
+			</section>
 
 			<section>temporary body</section>
 			<section>
