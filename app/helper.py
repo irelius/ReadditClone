02ec4efc786c077403d5656faa99a3ffa487211a
Ki Hong (Samuel) Bae <kihong711@hotmail.com>
2025-06-26 15:24:39 -0700

    Kb/comments backend (#13)
    * comments backend helper function fixed to not double dip comments if theyre
    
    * update comment seeder file to test out comment tree structure and fixed routes

--- app/helper.py
+++ app/helper.py
@@ -125,13 +125,30 @@ def return_post_likes(post_likes, action_type):
 def return_comments(comments):
     if comments[0] == None:
         return {"errors": ["Comment does not exist"]}, 404
+    
+    comment_by_id = []
+    all_comments = {}
+    
+    for comment in comments:
+        if comment.is_reply == False:
+            comment_by_id.append(comment.id)
+            all_comments[comment.id] = comment.to_dict()
 
+    return {
+        "comments_by_id": comment_by_id,
+        "all_comments": all_comments
+    }
+    
+def return_comments_flat(comments):
+    if comments[0] == None:
+        return {"errors": ["Comment does not exist"]}, 404
+    
     comment_by_id = []
     all_comments = {}
     
     for comment in comments:
         comment_by_id.append(comment.id)
-        all_comments[comment.id] = comment.to_dict()
+        all_comments[comment.id] = comment.flat_to_dict()
 
     return {
         "comments_by_id": comment_by_id,
@@ -141,9 +158,6 @@ def return_comments(comments):
 #  ------------------------- Comment Likes Helper Function -------------------------
 # returns "comment_likes_by_id" and "all_comment_likes"
 def return_comment_likes(comment_likes):
-    if comment_likes[0] == None:
-        return {"errors": ["Like on comment does not exist"]}, 404
-    
     comment_likes_by_id = []
     all_comment_likes = {}
     
